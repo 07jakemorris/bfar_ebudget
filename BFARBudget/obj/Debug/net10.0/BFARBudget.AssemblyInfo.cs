3259a5c6a731e9f521995fa86ab823a33f371742
07jakemorris <jakemorriscarino15@gmail.com>
2026-07-17 09:52:05 +0800

    nothing save here

--- BFARBudget/obj/Debug/net10.0/BFARBudget.AssemblyInfo.cs
+++ BFARBudget/obj/Debug/net10.0/BFARBudget.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BFARBudget")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880aa9c8a2fc1e903a2125d2086caa6097c39800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95dc0d630c8336c8e0eedabd8aba25c2c2349fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BFARBudget")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BFARBudget")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
